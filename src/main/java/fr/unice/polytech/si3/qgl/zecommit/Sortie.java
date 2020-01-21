52a1c7423063619364f56312cbb490be2c01dcc7
bertolottoloic <loic.bertolotto@etu.unice.fr>
2020-01-21 16:25:01 +0100

    Modification format sortie #10

--- src/main/java/fr/unice/polytech/si3/qgl/zecommit/Sortie.java
+++ src/main/java/fr/unice/polytech/si3/qgl/zecommit/Sortie.java
@@ -23,7 +23,6 @@ class Sortie{
 
     String afficheRound(List<Action> actions){
         try{
-            oM.configure(SerializationFeature.INDENT_OUTPUT, true);//Option pour ne pas afficher la sortie sur une seule ligne
             return oM.writeValueAsString(actions);
         } catch (IOException e){
             System.out.println(e.getMessage());
