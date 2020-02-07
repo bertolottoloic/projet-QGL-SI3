a80c544db21945a815fb1259ed4e629985ee7a19
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-02-07 16:50:43 +0100

    Ajout deserializer position reduit

--- src/main/java/fr/unice/polytech/si3/qgl/zecommit/deserializer/PositionDeserializer.java
+++ src/main/java/fr/unice/polytech/si3/qgl/zecommit/deserializer/PositionDeserializer.java
@@ -6,26 +6,13 @@ import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import fr.unice.polytech.si3.qgl.zecommit.boat.Position;
 
 
 import java.io.IOException;
-import java.util.concurrent.atomic.DoubleAccumulator;
 
 
-
-
-public class PositionDeserializer extends StdDeserializer<Position> {
-
-
-    protected PositionDeserializer(Class<?> vc) {
-        super(vc);
-    }
-
-    public PositionDeserializer(){
-        this(null);
-    }
+public class PositionDeserializer extends JsonDeserializer {
 
     @Override
     public Position deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
