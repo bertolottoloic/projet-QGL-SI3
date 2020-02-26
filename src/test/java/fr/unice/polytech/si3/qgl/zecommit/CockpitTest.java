ac3b84b917734990afbb517ef90450c2849a060d
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-02-26 19:22:06 +0100

    man

--- src/test/java/fr/unice/polytech/si3/qgl/zecommit/CockpitTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/zecommit/CockpitTest.java
@@ -11,6 +11,7 @@ class CockpitTest {
     Cockpit cockpit;
     String jsonInit;
     String jsonNR;
+    String jsonNextRound;
 
     @BeforeEach
     void setUp() {
@@ -108,12 +109,100 @@ class CockpitTest {
                 "    }\n" +
                 "  ]\n" +
                 "}";
+
+
+        jsonNextRound = "{\n" +
+                "  \"ship\": {\n" +
+                "    \"type\": \"ship\",\n" +
+                "    \"life\": 100,\n" +
+                "    \"position\": {\n" +
+                "      \"x\": 0,\n" +
+                "      \"y\": 0,\n" +
+                "      \"orientation\": 0\n" +
+                "    },\n" +
+                "    \"name\": \"Les copaings d'abord!\",\n" +
+                "    \"deck\": {\n" +
+                "      \"width\": 3,\n" +
+                "      \"length\": 6\n" +
+                "    },\n" +
+                "    \"entities\": [\n" +
+                "      {\n" +
+                "        \"x\": 1,\n" +
+                "        \"y\": 0,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 1,\n" +
+                "        \"y\": 2,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 3,\n" +
+                "        \"y\": 0,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 3,\n" +
+                "        \"y\": 2,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 4,\n" +
+                "        \"y\": 0,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 4,\n" +
+                "        \"y\": 2,\n" +
+                "        \"type\": \"oar\"\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 2,\n" +
+                "        \"y\": 1,\n" +
+                "        \"type\": \"sail\",\n" +
+                "        \"openned\": false\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"x\": 5,\n" +
+                "        \"y\": 0,\n" +
+                "        \"type\": \"rudder\"\n" +
+                "      }\n" +
+                "    ],\n" +
+                "    \"shape\": {\n" +
+                "      \"type\": \"rectangle\",\n" +
+                "      \"width\": 3,\n" +
+                "      \"height\": 6,\n" +
+                "      \"orientation\": 0\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"visibleEntities\": [\n" +
+                "    {\n" +
+                "      \"type\": \"stream\",\n" +
+                "      \"position\": {\n" +
+                "        \"x\": 500,\n" +
+                "        \"y\": 0,\n" +
+                "        \"orientation\": 0\n" +
+                "      },\n" +
+                "      \"shape\": {\n" +
+                "        \"type\": \"rectangle\",\n" +
+                "        \"width\": 50,\n" +
+                "        \"height\": 500,\n" +
+                "        \"orientation\": 0\n" +
+                "      },\n" +
+                "      \"strength\": 40\n" +
+                "    }\n" +
+                "  ],\n" +
+                "  \"wind\": {\n" +
+                "    \"orientation\": 0,\n" +
+                "    \"strength\": 110\n" +
+                "  }\n" +
+                "}";
         this.cockpit = new Cockpit();
         this.cockpit.initGame(jsonInit);
     }
     @Test
     void nextRoundTest() {
-        assertEquals("[{\"sailorId\":0,\"type\":\"OAR\"},{\"sailorId\":1,\"type\":\"OAR\"}]", this.cockpit.nextRound(jsonNR));
+        assertEquals("[{\"sailorId\":0,\"type\":\"OAR\"},{\"sailorId\":1,\"type\":\"OAR\"}]", this.cockpit.nextRound(jsonNextRound));
     }
 
 }
