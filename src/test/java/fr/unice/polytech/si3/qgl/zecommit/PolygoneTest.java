cb4daf2903525a7c6404f4c3fa5be215c42e2e4c
NathanMeulle <54913885+NathanMeulle@users.noreply.github.com>
2020-03-09 15:55:48 +0100

    Correction test

--- src/test/java/fr/unice/polytech/si3/qgl/zecommit/PolygoneTest.java
+++ src/test/java/fr/unice/polytech/si3/qgl/zecommit/PolygoneTest.java
@@ -24,6 +24,6 @@ public class PolygoneTest {
 
         Polygone polygone = new Polygone(0, points);
 
-        assertEquals(1.12, (double)Math.round(polygone.getRadius()*100)/100.0);
+        assertEquals(20 + 1.12, (double)Math.round(polygone.getRadius()*100)/100.0);
     }
 }
